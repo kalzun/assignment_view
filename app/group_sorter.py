72b27a89b58bfd60cf02b366ee28afc532186ba1
Rockland <erikand@gmail.com>
2020-09-04 16:29:32 +0200

    Sorter works to folders. Try Flask

---
+++ app/group_sorter.py
@@ -0,0 +1,138 @@
+from pathlib import Path
+from zipfile import ZipFile
+import csv
+import shutil
+import tempfile
+
+
+csvfile = '2020-09-03T1057_Karakterer-INFO132.csv'
+submissions_folder = 'submissions'
+
+positions = {
+    'name': 0,
+    'usercode': 3,
+    'group_info': 4,
+}
+
+COURSECODE = 'INFO132'
+N_OF_GROUPS = 22
+GROUPS = {}
+
+def get_submission_name(zipname: str) -> str:
+    '''
+    Extract the submission name from the name of the zipfile.
+    Regular zipfile-name could be:
+    '1599224423_837__INFO132-Temaoppgave_1_submissions.zip'
+    [random_number] [coursecode] [submission_name]
+    '''
+    start, end = zipname.find(COURSECODE), zipname.find('submissions')
+    if start == -1 or end == -1:
+        # Did not find what we were after.
+        return zipname.rstrip('.zip')
+    return zipname[start+len(COURSECODE):end].strip('-').rstrip('_')
+
+def get_zips() -> list:
+    app_dir = Path('.')
+    files = [str(f) for f in app_dir.glob('*.zip') if COURSECODE in f.name]
+    return files
+
+def unzip_file(filename) -> None:
+    if not Path(filename).exists():
+        return False
+    with ZipFile(filename, 'r') as zref:
+        tmp_dir = tempfile.TemporaryDirectory()
+        zref.extractall(tmp_dir.name)
+        sort_to_group_folders(tmp_dir.name, filename)
+
+
+def sort_to_group_folders(tmpdir_name: str, zipname: str='submission'):
+    # Ensure folder structure
+    # submission > "Temaoppgave1" > gruppenummer
+    submission_name = get_submission_name(zipname)
+    root_submission = Path(submissions_folder)
+    name_submission = root_submission / Path(submission_name)
+    if not root_submission.exists() and not name_submission.exists():
+        root_submission.mkdir()
+        name_submission.mkdir()
+
+    if not GROUPS:
+        build_group_overview()
+    for n, studentcodes in GROUPS.items():
+        to_folder = root_submission / Path(submission_name) / Path(f'{n}')
+        if not to_folder.exists():
+            to_folder.mkdir()
+        for studentcode in studentcodes:
+            filepaths = get_path_file_of_student(studentcode, Path(tmpdir_name))
+            # Some users have handed in multiple files, be sure to copy them all
+            if len(filepaths) == 0:
+                continue
+                # iterations = len(filepaths)
+            for i in range(len(filepaths)):
+                folder_file_str = root_submission.name + '/' + filepaths[i].name
+                shutil.copy(str(filepaths[i].resolve()), str(to_folder.resolve()))
+
+
+
+
+def copy_files_to_folder(studentcodes: set, group_num: int = 42):
+    '''
+    Files to be copied to a folder.
+    Create folder as child to submissions_folder
+    '''
+    root_dir = Path('.')
+    root_submission = Path(submissions_folder)
+
+    if root_submission.exists():
+        to_folder = root_submission / Path(submission_name) / Path(f'{group_num}')
+        if not to_folder.exists():
+            to_folder.mkdir()
+
+        for studentcode in studentcodes:
+            filepaths = get_path_file_of_student(studentcode)
+
+            # Some users have handed in multiple files, be sure to copy them all
+            iterations = 1
+            if len(filepaths) > 1:
+                iterations = len(filepaths)
+            for i in range(iterations):
+                folder_file_str = root_submission.name + '/' + filepaths[i].name
+                shutil.copy(str(filepaths[i].resolve()), str(to_folder.resolve()))
+
+
+def get_path_file_of_student(studentcode: str, folder: Path):
+    # folder = Path(submissions_folder)
+    if folder.exists():
+        files = [child for child in folder.iterdir() if studentcode in child.name.lower()]
+        # if len(files) > 1:
+        #     raise Exception(f'Found {len(files)} files when searching for file containing "{studentcode}".')
+        return files
+
+def build_group_overview():
+    for n in range(N_OF_GROUPS):
+        groupset = find_group(n)
+        if len(groupset) <= 0:
+            continue
+        if n in GROUPS:
+            GROUPS[n] = GROUPS[n].update(groupset)
+        else:
+            GROUPS[n] = groupset
+
+def find_group(group_number: int) -> set:
+    group_name = 'Gruppe ' + str(group_number)
+    group_set = set()
+    with open(csvfile, 'r') as fh:
+        content = csv.reader(fh)
+        for i, line in enumerate(content):
+            if group_name in line[positions['group_info']]:
+                if 'Teststudent' == line[positions['name']]:
+                    continue
+                group_set.add(
+                    line[positions['usercode']]
+                )
+
+    return group_set
+
+
+if __name__ == '__main__':
+    filename = '1599122728_293__INFO132-Temaoppgave_1_submissions.zip'
+    unzip_file(filename)
