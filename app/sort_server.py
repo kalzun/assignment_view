a842e6470dcc1bd1490c625a860872dadcc2fa41
Rockland <erikand@gmail.com>
2020-10-06 00:48:32 +0200

    Add last update visually

--- app/sort_server.py
+++ app/sort_server.py
@@ -4,7 +4,12 @@ from flask import Flask, render_template, send_from_directory, redirect, url_for
 from textwrap import TextWrapper
 import json
 from pathlib import Path
+from time import ctime
+from .group_sorter import LOGFOLDER, LOGFILENAME, get_submission_name
+from dotenv import load_dotenv
 
+dotenv_path = Path(__file__) / '.env'  # Path to .env file
+load_dotenv(dotenv_path)
 
 
 app = Flask(__name__,
@@ -17,10 +22,6 @@ app = Flask(__name__,
 # To run the development server:
 # export FLASK_APP=sort_server.py
 # flask run
-app.config.update(
-    TESTING=True,
-    TEMPLATES_AUTO_RELOAD = True,
-)
 
 app.config.from_object(__name__)
 
@@ -47,8 +48,12 @@ def get_folders(folder='', group=0):
     except ValueError:
         folders = sorted([fo.name for fo in theme_dir.iterdir() if fo.is_dir()])
     files = sorted([f.name for f in theme_dir.iterdir() if f.is_file()])
-    return render_template('filebrowser.html', context={'folders': folders,
-                                                       'files': files})
+    return render_template('filebrowser.html',
+                         context={
+                             'folders': folders,
+                             'files': files,
+                             'update_info': get_latest_update_info()
+                         })
 
 @app.route('/submissions/<folder>/<int:group>/<filename>')
 def get_specific_file(folder, group, filename):
@@ -58,7 +63,44 @@ def get_specific_file(folder, group, filename):
     filepath = group_dir.joinpath(filename)
     with open(filepath, 'r') as f:
         content = ''.join(f.readlines())
-        return render_template('fileviewer.html', context=content, filename=get_studentcode_from_filename(filename))
+        return render_template(
+            'fileviewer.html',
+            context=content,
+            studentcode=get_studentcode_from_filename(filename))
+
+
+def get_latest_update_info():
+    '''
+    Get the latest log_update
+    for information to the user of when (and which submission)
+    the latest file was unzipped.
+    returns a tuple of submission name and a ctime-formatted time.
+    '''
+    with open(LOGFOLDER / LOGFILENAME) as logfile:
+        for line in logfile.readlines()[::-1]:
+            # Due to logging not setup "correctly", this "improvable" double check in if:
+            if '.zip' in line and 'Unzipping' in line:
+                submission_name = get_submission_name(line)
+                return submission_name, get_zip_file_dateinfo_from_log(submission_name)
+
+
+def get_zip_file_dateinfo_from_log(submission_name):
+    '''
+    Parse the logfile entry to get the datetime
+    '''
+    with open(LOGFOLDER / LOGFILENAME) as logfile:
+        for line in logfile.readlines()[::-1]:
+            if submission_name in line and '.zip' in line:
+                return line[5:21]
+
+
+def get_file_update_info(filepath):
+    '''
+    Return the time of last update of this file in seconds.
+    Converted to ctime
+    '''
+    return ctime(Path(filepath).stat().st_mtime)
+
 
 
 
