14babdc03c28316a9f80eb5b2e532ccbe44ef85c
Rockland <erikand@gmail.com>
2021-05-21 09:28:30 +0200

    Small mod to run to remove a log error

--- app/run.sh
+++ app/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # if [[ "$OSTYPE" == "msys" ]]; then
 #     set
-python -c "import webbrowser as wb; wb.open('localhost:5000')" & flask run
+python -c "import webbrowser as wb; wb.open('http://localhost:5000')" & flask run
